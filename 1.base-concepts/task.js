b53972ee728eeadceeb292ff3daf9ee8a7e0df1d
Sergei US <ustimenko.sv@gmail.com>
2022-11-11 01:55:49 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -34,29 +34,29 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
   if (isNaN(percent)) {
     return 'Параметр "Процентая ставка" содержит неправильное значение ' + '"' + percent + '"' + '.';
-  } else if (isNaN(contribution)) {
-    return 'Параметр "Начальный взнос" содержит неправильное значение ' + '"' + contribution + '"' + '.';
-  } else if (isNaN(amount)) {
-    return 'Параметр "Общая стоимость" содержит неправильное значение ' + '"' + amount + '"' + '.';
-  } else {
-    let creditPeriodInMilliseconds = new Date(datePayCredit - dateNow);
-    let creditPeriodInMonths = Math.floor(creditPeriodInMilliseconds / millisecondsInMonth);
-    let n = creditPeriodInMonths;
-    totalAmount = (s * (p + p / (((1 + p) ** n) - 1)) * n).toFixed(2);
-  return totalAmount;
+    } else if (isNaN(contribution)) {
+      return 'Параметр "Начальный взнос" содержит неправильное значение ' + '"' + contribution + '"' + '.';
+    } else if (isNaN(amount)) {
+      return 'Параметр "Общая стоимость" содержит неправильное значение ' + '"' + amount + '"' + '.';
+    } else {
+      let creditPeriodInMilliseconds = new Date(datePayCredit - dateNow);
+      let creditPeriodInMonths = Math.floor(creditPeriodInMilliseconds / millisecondsInMonth);
+      let n = creditPeriodInMonths;
+      totalAmount = (s * (p + p / (((1 + p) ** n) - 1)) * n).toFixed(2);
+    return totalAmount;
   }
 }
 
-  function months(m) {
-    let now = new Date();
-    now.setMonth(now.getMonth() + m);
-    return now;
-  }
+function months(m) {
+  let now = new Date();
+  now.setMonth(now.getMonth() + m);
+  return now;
+}
   
-  console.log(calculateTotalMortgage(10, 0, 50000, months(12)));
-  console.log(calculateTotalMortgage(10, 1000, 50000, months(12)));
-  console.log(calculateTotalMortgage(10, 0, 20000, months(24)));
-  console.log(calculateTotalMortgage(10, 1000, 20000, months(24)));
-  console.log(calculateTotalMortgage(10, 20000, 20000, months(24)));
-  console.log(calculateTotalMortgage(10, 0, 10000, months(36)));
-  console.log(calculateTotalMortgage(15, 0, 10000, months(36)));
+console.log(calculateTotalMortgage(10, 0, 50000, months(12)));
+console.log(calculateTotalMortgage(10, 1000, 50000, months(12)));
+console.log(calculateTotalMortgage(10, 0, 20000, months(24)));
+console.log(calculateTotalMortgage(10, 1000, 20000, months(24)));
+console.log(calculateTotalMortgage(10, 20000, 20000, months(24)));
+console.log(calculateTotalMortgage(10, 0, 10000, months(36)));
+console.log(calculateTotalMortgage(15, 0, 10000, months(36)));
