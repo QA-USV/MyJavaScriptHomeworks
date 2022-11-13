d3f6b4e9312b50fcc1ba8868d98449f690e3a6eb
Sergei US <ustimenko.sv@gmail.com>
2022-11-13 19:47:35 +0300

    Final commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,56 +1,39 @@
-
 // Задание 1
-'use strict';
 
-function getArrayParams(...array) {
-  let sum = 0; 
-  let avg = 0;
-  let min = +array[0];
-  let minEdge = -100;
-  let max = +array[array.length - 1];
-  let maxEdge = 100;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] < minEdge || array[i] > maxEdge) {
-      return (`'${array[i]}' is out of range from ${minEdge} to ${maxEdge}.`);
-    } 
-    if (array[i] < min) {
-      min = array[i];
-    } 
-    if (array[i] > max) {
-      max = array[i];
-    }
-    sum += array[i];
-    avg = (sum / array.length).toFixed(2);
-    avg = +avg;
-    }
+'use strict';
+function getArrayParams(arr) {
+  const initialSum = 0;
+  const sum = +arr.reduce((partialSum, elem) => partialSum + elem, initialSum); 
+  let max = +arr.reduce((partialSum, elem) => Math.max(partialSum, elem), -Infinity);
+  let min = +arr.reduce((partialSum, elem) => Math.min(partialSum, elem), Infinity);
+  let avg = +(sum / arr.length).toFixed(2);
+
   return { min: min, max: max, avg: avg };
 }
-
+  
 // Задание 2
-function worker(...array) {
-  let sum = 0;
-  for (let value of array) {
-    sum += value;
-  }
+function worker(arr) {
+  const initialSum = 0;
+  const sum = +arr.reduce((partialSum, elem) => partialSum + elem, initialSum);
   return sum;
 }
 
-function makeWork(...arrOfArr) {
-  let max = 0; 
-  func = worker;
-  for (let i = 0; i < arrOfArr.length; i++) {
-      if (worker(arrOfArr[i]) > max) {
-        max = func(array);
-      }
-    }
+function makeWork(arrOfArr, worker) {
+  let max = +arrOfArr.reduce((a, b) => Math.max(worker(a), worker(b)));
   return max;
 }
-console.log(makeWork([1, 2, 3, 4], [10, 20, -10, -20]));
-
 
 
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  const maxInArray = +arr.reduce((partialSum, elem) => Math.max(partialSum, elem), -Infinity);
+  const minInArray = +arr.reduce((partialSum, elem) => Math.min(partialSum, elem), Infinity);
+  const absInArray = Math.abs(maxInArray - minInArray);
+  return absInArray;
+}
+
+function makeWork(arrOfArr, worker2) {
+  const abs = +arrOfArr.reduce((a, b) => Math.max(worker2(a), worker2(b)));
+  return abs;
 }
