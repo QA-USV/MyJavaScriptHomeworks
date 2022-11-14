15fd101a303958969ee8b2b4865b32d54acba953
Sergei US <ustimenko.sv@gmail.com>
2022-11-14 19:42:09 +0300

    Initial commi

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,27 @@
-function compareArrays(arr1, arr2) {
-  let result;
+'use strict';
 
-  // Ваш код
+//Task 1
+//First variant 
+// function compareArrays(arr1, arr2) {
+//   if (arr1.length === arr2.length) {
+//     const result = arr1.every((value, index) => 
+//     arr1[value] === arr2[value] && 
+//     arr1[index] === arr2[index]);
+//   return result;
+//   } else {
+//     return false;
+//   }
+// }
 
-  return result; // boolean
-}
+//Task 1
+//Second variant 
+const compareArrays = (arr1, arr2) => 
+arr1.length === arr2.length && 
+arr1.every((value, index) => value === arr2[index]);
 
+
+//Task 2
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter(value => value > 0).filter(value => value % 3 === 0).map(value => value * 10);
+  return resultArr;
 }
