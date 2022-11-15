1987c1923a353e9ae29b92542b591ac24734623c
Sergei US <ustimenko.sv@gmail.com>
2022-11-15 21:58:08 +0300

    Final commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,42 @@
-function Student(name, gender, age) {
-    // Ваш код
+'use strict';
 
+class Student {
+  constructor(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+  }
+  
+  setSubject(subjectName) {
+    this.subject = subjectName;
+  }
+  
+  addMark(...mark) {
+    if (this.marks === undefined) {
+      this.marks = mark;
+      } else {
+      this.marks = [...this.marks, ...mark];
+      }
+  }
+    
+  addMarks(...arr) {
+    if (this.marks === undefined) {
+      this.marks = arr;
+    } else {
+      this.marks = [...this.marks, ...arr];
+    }
+  }
+
+  getAverage() {
+    const sum = this.marks.reduce((el, acc) => acc += el, 0);
+    const avg = +(sum / this.marks.length); 
+    // More readable result is '+(sum / this.marks.length).toFixed(2)'. But the result will not meet the expected one. //
+    return avg;
+    }
+  
+  exclude(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
 }
-
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
-
-// ваш код для остальных методов
